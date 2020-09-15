1c21808ac2b1669e26039cf8704cf483e2fb834f
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-09-15 14:14:15 -0500

    Create listtoarray.js

---
+++ listtoarray.js
@@ -0,0 +1,16 @@
+// obtenir les proprietes d'un objet, (to get object properties.
+
+/*  recup keys d'un object
+function nbre_obj(){
+ var j = 0;
+ var tab = [];
+ var test = {"a":1, "b":2 ,3:4};
+ //var recup = Object.keys(test);
+ //return recup.length;
+ for(var i in test){
+    tab[tab.length] = i;
+    j++;
+ }
+ return tab;
+}
+nbre_obj(); */
